ebcdbfa98d501af7050a888bd2b8904c5d3ae4b7
avshrs <sebastian.antosiak@gmail.com>
2017-11-24 15:01:01 +0100

    autocommit on change

--- lib/lib_maapi_gpio_pi_4_0.py
+++ lib/lib_maapi_gpio_pi_4_0.py
@@ -6,7 +6,7 @@ import psycopg2
 import RPi.GPIO as GPIO
 import sys
 import lib.MaaPi_DB_connection as maapidb
-
+import logging
 
 class class_get_values(object):
     debug = 2
@@ -14,118 +14,105 @@ class class_get_values(object):
     @classmethod
     def _debug(self, level, msg):
         if self.debug >= level:
-            print("DEBUG OneWire_GPI0 {0} {1}, {2}".format(
-                level, datetime.now(), msg))
+            print("DEBUG OneWire_GPI0 {0} {1}, {2}".format(level, datetime.now(), msg))
+            logging.debug("DEBUG OneWire_GPI0 {0} {1}, {2}".format(level, datetime.now(), msg))
+
 
     @staticmethod
     def set_gpio_state(mp_table, slonv):
         pass
 
 
+    @classmethod
+    def check_value(self, mp_table, arg, device_last_value, device_range_value, min_max):
+        value = 1
+        test = 0
+        if mp_table[arg[0]]["switch_value_{0}_e".format(min_max)]:
 
+            """ IF REFERENCE SENSOR IS ON AND ID EXIST """
+            if mp_table[arg[0]]["switch_reference_sensor_{0}_e".format(min_max)] and mp_table[arg[0]]["switch_reference_sensor_{0}_id".format(min_max)]:
 
+                self._debug(1, "switch_reference_sensor_{0}_e and switch_reference_sensor_{0}_id is True".format(min_max))
+                device_range_value_ref = maapidb.MaaPiDBConnection().select_last_nr_of_values(mp_table[arg[0]]["switch_reference_sensor_{0}_id".format(min_max)], mp_table[arg[0]]["switch_range_acc"])
 
+                for i in range(0, mp_table[arg[0]]["switch_range_acc"]):
+                    if device_range_value_ref[i] is not 99999 and device_range_value[i] is not 99999:
+                        if min_max == "min":
+                            if device_range_value[i] < device_range_value_ref[i] - mp_table[arg[0]]["switch_value_{0}".format(min_max)]:
+                                self._debug(2, "val_{0}  = 1".format(min_max))
+                                test +=1
+                            else:
+                                value = 0
+                                self._debug(2, "val_{0}  = 0 ".format(min_max))
+                        elif min_max == "max":
+                            if device_range_value[i] > device_range_value_ref[i] + mp_table[arg[0]]["switch_value_{0}".format(min_max)]:
+                                self._debug(2, "val_{0}  = 1".format(min_max))
+                                test +=1
+                            else:
+                                value = 0
+                                self._debug(2, "val_{0}  = 0 ".format(min_max))
+
+                        else:
+                            self._debug(2, "error min_max not define")
+                self._debug(2, "Down state is == {0}".format(test))
+                self._debug(1, "finally val_{1}  = {0} ".format(value,min_max))
+
+            else:
+                """ IF MINIMAL REFERENCE SENSOR IS OFF"""
+                self._debug( 1, "switch_reference_sensor_{0}_e and switch_reference_sensor_{0}_id is False".format(min_max))
+
+                for i in range(mp_table[arg[0]]["switch_range_acc"]):
+                    self._debug(2, "source {0} <  val {1} = {2}".format(
+                                                    device_range_value[i],
+                                                    mp_table[arg[0]]["switch_value_{0}".format(min_max)],
+                                                    device_range_value[i] < mp_table[arg[0]]["switch_value_{0}".format(min_max)]))
+
+                    if min_max == "min":
+                        if device_range_value[i] < device_range_value_ref[i]:
+                            self._debug(2, "val_{0}  = 1".format(min_max))
+                            test +=1
+                        else:
+                            value = 0
+                            self._debug(2, "val_{0}  = 0 ".format(min_max))
+                    elif min_max == "max":
+                        if device_range_value[i] > device_range_value_ref[i]:
+                            self._debug(2, "val_{0}  = 1".format(min_max))
+                            test +=1
+                        else:
+                            value = 0
+                            self._debug(2, "val_{0}  = 0 ".format(min_max))
+                    else:
+                        self._debug(2, "error min_max not define")
+
+                self._debug( 1, "finally val_min  = {0} ".format(val_min))
+                self._debug(2, "Down state is == {0}".format(test))
+        return value
 
     @classmethod
     def __init__(self, *args):
+        logging.basicConfig(filename='lib_maapi_gpio.log',level=logging.DEBUG)
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
         mp_table = maapidb.MaaPiDBConnection().table("maapi_switch").columns(
             'switch_update_rom_id', "*").filters_eq(switch_enabled=True).get()
+
+
+
         device_last_value = maapidb.MaaPiDBConnection().table(
             "devices").columns('dev_id', 'dev_value', 'dev_gpio_pin').get()
+
         self._debug(3, "get device_last_value ={0}".format(device_last_value))
         for arg in args:
-
+            device_range_value = maapidb.MaaPiDBConnection().select_last_nr_of_values(mp_table[arg[0]]["switch_data_from_sens_id"], mp_table[arg[0]]["switch_range_acc"])
             if mp_table[arg[0]]["switch_data_from_sens_id"] and mp_table[arg[0]]["switch_update_rom_id"]:
                 self._debug(1, "switch_data_from_sens_id and switch_update_rom_id  is TRUE")
-                val_max = 1
-                val_min = 1
+
 
                 device_range_value = maapidb.MaaPiDBConnection().select_last_nr_of_values(mp_table[arg[0]]["switch_data_from_sens_id"], mp_table[arg[0]]["switch_range_acc"])
                 self._debug(1, "get switch_range_acc = {0}".format(device_range_value))
-
-                """ IF MINIMAL VALUE IS ON """
-                if mp_table[arg[0]]["switch_value_min_e"]:
-
-                    """ IF MINIMAL REFERENCE SENSOR IS ON AND ID EXIST """
-                    if mp_table[arg[0]]["switch_reference_sensor_min_e"] and mp_table[arg[0]]["switch_reference_sensor_min_id"]:
-
-                        self._debug(1, "switch_reference_sensor_min_e and switch_reference_sensor_min_id is True".format())
-                        device_range_value_ref_min = maapidb.MaaPiDBConnection().select_last_nr_of_values(mp_table[arg[0]]["switch_reference_sensor_min_id"], mp_table[arg[0]]["switch_range_acc"])
-
-                        for i in range(0, mp_table[arg[0]]["switch_range_acc"]):
-                            if device_range_value_ref_min[i] is not 99999 and device_range_value[i] is not 99999:
-                                if device_range_value[i] < device_range_value_ref_min[i] - mp_table[arg[0]]["switch_value_min"]:
-                                    self._debug(2, "val_min  = 1")
-                                else:
-                                    val_min = 0
-                                    self._debug(2, "val_min  = 0 ")
-
-                        self._debug(1, "finally val_min  = {0} ".format(val_min))
-
-                    else:
-
-                        """ IF MINIMAL REFERENCE SENSOR IS OFF"""
-                        self._debug( 1, "switch_reference_sensor_min_e and switch_reference_sensor_min_id is False".format())
-                        for i in range(mp_table[arg[0]]["switch_range_acc"]):
-                            self._debug(2, "source {0} <  val {1} = {2}".format(
-                                device_range_value[i],  mp_table[arg[0]]["switch_value_min"], device_range_value[i] < mp_table[arg[0]]["switch_value_min"]))
-                            if device_range_value[i] < mp_table[arg[0]]["switch_value_min"]:
-                                self._debug(2, "val_min  = 1")
-                            else:
-                                val_min = 0
-                                self._debug(2, "val_min  = 0 ")
-                        self._debug(
-                            1, "finally val_min  = {0} ".format(val_min))
-
-                """ IF MAXIMUM VALUE IS ON """
-                if mp_table[arg[0]]["switch_value_max_e"]:
-                    """ IF MAXIMUM REFERENCE SENSOR IS ON AND ID EXIST """
-                    if mp_table[arg[0]]["switch_reference_sensor_max_e"] and mp_table[arg[0]]["switch_reference_sensor_max_id"]:
-
-                        self._debug(
-                            1, "switch_reference_sensor_max_e and switch_reference_sensor_max_id is True".format())
-                        device_range_value_ref_max = maapidb.MaaPiDBConnection().select_last_nr_of_values(
-                                                mp_table[arg[0]]["switch_reference_sensor_max_id"],
-                                                mp_table[arg[0]]["switch_range_acc"])
-
-                        self._debug(1, "get device_range_value_ref_max = {0}".format(
-                            device_range_value_ref_max))
-                        for i in range(mp_table[arg[0]]["switch_range_acc"]):
-                            if device_range_value_ref_max[i] is not 99999 and device_range_value[i] is not 99999:
-                                self._debug(2, "source {0} > ref {1} + val {2} = {3}".format(
-                                                device_range_value[i],
-                                                device_range_value_ref_max[i],
-                                                mp_table[arg[0]]["switch_value_max"],
-                                                device_range_value[i] > device_range_value_ref_max[i] + mp_table[arg[0]]["switch_value_max"]))
-
-                                if device_range_value[i] > device_range_value_ref_max[i] + mp_table[arg[0]]["switch_value_max"]:
-                                    self._debug(2, "val_max  = 1")
-                                else:
-                                    val_max = 0
-                                    self._debug(2, "val_max  = 0 ")
-                        self._debug(
-                            1, "finally val_max  = {0} ".format(val_max))
-                    else:
-                        """ IF MAXIMUM REFERENCE SENSOR IS OFF"""
-                        self._debug(
-                            1, "switch_reference_sensor_max_e and switch_reference_sensor_max_id is False".format())
-                        for i in range(mp_table[arg[0]]["switch_range_acc"]):
-
-                            self._debug(2, "source {0} >  val {1} = {2}".format(
-                                device_range_value[i],  mp_table[arg[0]]["switch_value_max"], device_range_value[i] > mp_table[arg[0]]["switch_value_max"]))
-                            if device_range_value[i] > mp_table[arg[0]]["switch_value_max"]:
-                                self._debug(2, "val_max  = 1")
-                            else:
-                                val_max = 0
-                                self._debug(2, "val_max  = 0 ")
-                        self._debug(
-                            1, "finally val_max  = {0} ".format(val_max))
-
-
-
+                val_max = self.check_value(mp_table, arg, device_last_value, device_range_value, "min")
+                val_min = self.check_value(mp_table, arg, device_last_value, device_range_value, "max")
 
 
 
@@ -139,18 +126,8 @@ class class_get_values(object):
                             1, "Table switch_turn_on_at_sensor_e and switch_turn_on_at_sensor_id is True".format())
 
                         if device_last_value[mp_table[arg[0]]["switch_turn_on_at_sensor_id"]]["dev_value"] < mp_table[arg[0]]["switch_turn_on_at_sensor_value_min"]:
-                            self._debug(1, "switch_turn_on_at_sensor_e condition on min {0} < {1} is {2} put normal val_min {3}".format(
-                                            device_last_value[mp_table[arg[0]]["switch_turn_on_at_sensor_id"]]["dev_value"],
-                                            mp_table[arg[0]]["switch_turn_on_at_sensor_value_min"],
-                                            device_last_value[mp_table[arg[0]]["switch_turn_on_at_sensor_id"]]["dev_value"] < mp_table[arg[0]]["switch_turn_on_at_sensor_value_min"],
-                                            val_min))
-
+                            pass
                         else:
-                            self._debug(1, "switch_turn_on_at_sensor_e condition on min {0} < {1} is {2} put forced val_min to {3}".format(
-                                            device_last_value[mp_table[arg[0]]["switch_turn_on_at_sensor_id"]]["dev_value"],
-                                            mp_table[arg[0]]["switch_turn_on_at_sensor_value_min"],
-                                            device_last_value[mp_table[arg[0]]["switch_turn_on_at_sensor_id"]]["dev_value"] < mp_table[arg[0]]["switch_turn_on_at_sensor_value_min"],
-                                            mp_table[arg[0]]["switch_turn_on_at_cond_not_val"]))
                             val_max = mp_table[arg[0]]["switch_turn_on_at_cond_not_val"]
                             val_min = mp_table[arg[0]]["switch_turn_on_at_cond_not_val"]
 
