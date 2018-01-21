4f6431af29f419344520910618c5b0cdde5272c7
avshrs <sebastian.antosiak@gmail.com>
2018-01-21 23:01:01 +0100

    autocommit on change

--- lib/lib_maapi_bme280_pi_4_0.py
+++ lib/lib_maapi_bme280_pi_4_0.py
@@ -15,23 +15,23 @@ class class_get_values(object):
     @classmethod
     def __init__(self,*args):
         for arg in args:
-
-                if arg[2] == "BME280_Temp_pi":
+            try:
+                if arg[2] == "BME280_Temperature_pi":
                     sensor = BME280(t_mode=BME280_OSAMPLE_8)
                     temp = sensor.read_temperature()
                     maapidb.MaaPiDBConnection.insert_data(arg[0],temp,arg[2],True)
-                    print ("temp readed {0}".format(temp))
+                    self._debug(1,"\Pressure = {0}".format(temp))
                 if arg[2] == "BME280_Pressure_pi":
                     sensor2 = BME280(p_mode=BME280_OSAMPLE_8)
                     pressure = float(sensor2.read_pressure())
                     maapidb.MaaPiDBConnection.insert_data(arg[0],pressure/100,arg[2],True)
-                    print ("pressure readed {0}".format(pressure))
-
+                    self._debug(1,"\Pressure = {0}".format(pressure))
                 if arg[2] == "BME280_Humidity_pi":
                     sensor3 = BME280(h_mode=BME280_OSAMPLE_8)
                     hum = float(sensor3.read_humidity())
                     maapidb.MaaPiDBConnection.insert_data(arg[0],hum,arg[2],True)
-                    print ("temp readed {0}".format(hum))
+                    self._debug(1,"\Humidity = {0}".format(hum))
 
-
-            
+            except:
+                self._debug(1,"\tERROR reading values from rom_id[1]: {0}".format(arg[1]))
+                maapidb.MaaPiDBConnection.insert_data(arg[0],0,arg[2],False)
