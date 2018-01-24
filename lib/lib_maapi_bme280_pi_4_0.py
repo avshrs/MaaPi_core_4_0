90a8685a1bb0572943f822d1592e8b9b486af29b
avshrs <sebastian.antosiak@gmail.com>
2018-01-24 11:01:01 +0100

    autocommit on change

--- lib/lib_maapi_bme280_pi_4_0.py
+++ lib/lib_maapi_bme280_pi_4_0.py
@@ -21,19 +21,19 @@ class class_get_values(object):
                 sensor = BME280(p_mode=BME280_OSAMPLE_8)
                 temp = float(sensor.read_temperature())
                 maapidb.MaaPiDBConnection.insert_data(s_id, temp, name, True)
-                self._debug(1,"Temperature = {0}".format(temp))
+                self._debug(1,"Temperature = {0} ".format(temp))
 
             if name == "BME280_Pressure_pi":
                 sensor2 = BME280(p_mode=BME280_OSAMPLE_8)
                 pressure = float(sensor2.read_pressure())
                 maapidb.MaaPiDBConnection.insert_data(s_id, pressure / 100, name, True)
-                self._debug(1,"Pressure = {0}".format(pressure))
+                self._debug(1,"Pressure = {0} ".format(pressure))
 
             if name == "BME280_Humidity_pi":
                 sensor3 = BME280(h_mode=BME280_OSAMPLE_8)
                 hum = float(sensor3.read_humidity())
                 maapidb.MaaPiDBConnection.insert_data(s_id, hum, name, True)
-                self._debug(1,"Humidity = {0}".format(hum))
+                self._debug(1,"Humidity = {0} ".format(hum))
         except:
             self._debug(1, "\tERROR reading values from: {0}".format(s_id))
             maapidb.MaaPiDBConnection.insert_data(s_id, 0, name, False)
