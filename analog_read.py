ebd7edd003906a301b912ee94c1d1099524fc712
avshrs <sebastian.antosiak@gmail.com>
2019-01-07 00:01:01 +0100

    autocommit on change

---
+++ analog_read.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import smbus
+import time
+import curses
+ 
+bus = smbus.SMBus(1)
+
+aout = 0
+
+for a in range(0,4):
+      aout = aout + 1
+      bus.write_byte_data(0x48,0x40 | ((a+1) & 0x03), aout)
+      v = bus.read_byte(0x48)
+      #hashes = v / 4
+      #spaces = 64 - hashes
+      print  str(v)
+      
+time.sleep(0.1)
+
+ 
