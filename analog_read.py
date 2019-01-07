616851e9ef4f154ef8be46c9dcc87f685ae4d9a3
avshrs <sebastian.antosiak@gmail.com>
2019-01-07 08:01:01 +0100

    autocommit on change

--- analog_read.py
+++ analog_read.py
@@ -8,14 +8,26 @@ bus = smbus.SMBus(1)
 
 aout = 0
 
+def convert(input):
+   return input*(16.5/256)
+   
+
+
 for a in range(0,4):
-      aout = aout + 1
-      bus.write_byte(0x48,a)
-      v = bus.read_byte(0x48)
-      #hashes = v / 4
-      #spaces = 64 - hashes
-      print  str(v)
+   addr = 0x40 | a
+   bus.write_byte(0x48,a)
+   time.sleep(0.01)
+   reading = bus.read_byte(0x48)
+
+   conV = convert(reading)
+   conA = conV/0.033333
+   conW = conA*235
+   print "{0}: 235V - {1}A = {2}W".format(a,conA, conW)
       
-time.sleep(0.1)
+#xtime.sleep(0.1)
 
  
+#7 6 5 4 3 2 1 0
+#0 A B B 0 C D D 
+
+#0 0 0 0 0
