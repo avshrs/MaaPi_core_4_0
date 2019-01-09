e8550ecf68ec94abfbf259dfec20209f059d5b0f
avshrs <sebastian.antosiak@gmail.com>
2019-01-09 23:01:01 +0100

    autocommit on change

--- b_analog.py
+++ b_analog.py
@@ -4,86 +4,61 @@ from smbus2 import SMBus, i2c_msg
 import time
 import datetime as dt
 import os
-bus = SMBus(1)
-sens = (0,1,2,3)
-start =dt.datetime.now()
-inter = 3
-
-pause = 0.001
 
 
-def read(sensor):
-   counter = 3
-   accuracy = 50
-   bus.write_byte(0x48,0x04)
-   counter = inter
-   out = []
-   for ix in range(0,accuracy):
-      data = bus.read_i2c_block_data(0x48,int(sensor),32)[1:]
-      if  data[1] > 1 and data[2] > 1:
-         for dat in data:
-             out.append(dat)
-         counter -= 1
-         if counter < 1:
-            break
-   return out
+class pcfxxxxi2(object):
+   bus = SMBus(1)
+   def read(self,sensor):
+      counter = 10
+      accuracy = 20
+      self.bus.write_byte(0x48,0x04)
+      out = []
+      for ix in range(0,accuracy):
+         data = self.bus.read_i2c_block_data(0x48,int(sensor),32)[1:]
+         if  data[1] > 1 and data[2] > 1:
+            out.append(max(data))
+            counter -= 1
+            if counter < 1:
+               break
+         if len(out) < 1:
+            out = [0]
+      #print out   # print printprintprintprintprint
+      return out
 
-def avg(data):
-    data_ = 0.0
-    for i in data:
-        data_+= float(i)
-    return data_/(len(data))
+   def avg(self,data):
+      data_ = 0
+      for i in data:
+         data_+= i
+      return data_/(len(data))
 
-def toV(data):
-   out = [0,0,0,0]
-   vcc = 2.29
-   ratio = vcc / 256.0
-   for i in range(0,4): 
-      out[i] = avg(data[i])*ratio
-   return out
+   def convert(self,data,type_):
+      factor = 2.29 / 256.0  # pfc factor
+      dataAvg = max(data)
+      if type_ != 0:
+         volts  = dataAvg * factor 
+         ampers = volts / 0.0333333
+         wats   = ampers * 235.0
+         volts  = 0
+      else: 
+         volts  = (dataAvg * factor )*190
+         ampers = 0
+         wats   = 0
+      return volts,ampers,wats
 
-def toA(data):
-   for i in range(1,4):
-      if data[i] > 0 and data[i]<1.3:
-         data[i] = data[i] / 0.0333333
-   return data
 
-def toW(data):
-   ratio = 234
-   for i in range(1,4):
-      data[i]*=ratio
-   return data
+   def __init__(self,args):
+      for iii in range(0,4):
+         data = self.read(iii)
+         volt, amper, wat = self.convert(data,iii)
+         print ("{0}\t volts= {1:.1f} \tampers= {2:.1f} \twats= {3:.1f} ".format(iii,volt,amper,wat))
 
-def toVolts(data): 
-   ratio = 190
-   data[0]*=190
-   return data
 
-pause = 0.001
-data=[[],[],[],[]]
-for x in range(0,4):
-    readings = read(x)
-  
+start = dt.datetime.now()
+read = pcfxxxxi2(1) 
+
+stop = dt.datetime.now()
+print stop-start
+
+      
+
    
-print data
-volts = toV(data)
-print volts
-ampers = toA(volts)
-print ampers
-wats = toW(ampers)
-print wats
-out = toVolts(wats)
-print out
-
-stop =dt.datetime.now()
-print stop - start
-for i in sens:
-    if i != 0:
-        print ("{0}\t max_read= {1}\t volts= {2:.1f} \tampers= {3:.1f} \twats= {4:.1f} ".format(i,volts[i],volts[i],ampers[i],wats[i]))
-    else:
-        print "{0}\t max_read= {1} \t\t\t\t\tsieci ={2:.2f}".format(i,volts[i],out[i])
-
-
-    
-
- 
--- lib/lib_maapi_pcf8591_i2c.py
+++ lib/lib_maapi_pcf8591_i2c.py
@@ -3,79 +3,65 @@
 import sys
 from datetime import datetime
 import lib.MaaPi_DB_connection as maapidb
-import smbus
+from smbus2 import SMBus, i2c_msg
 import time
 
 
 class class_get_values(object):
-    debug = 0
-    device = 0x48
-    supplayVolts = 235
-    bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
-
-    acc = 200
-    sens=    [0,1,2,3]
-    dataOut= [0,0,0,0]
-    volts =  [0,0,0,0]
-    ampers = [0,0,0,0]
-    wats =   [0,0,0,0]
-
+    bus = SMBus(1)
     @classmethod
-    def _debug(self, level, msg):
-        if self.debug >= level:
-            print("DEBUG BH1750 {0} {1}, {2}".format(level, datetime.now(),msg))
-
+    def read(self,sensor):
+        counter = 10
+        accuracy = 20
+        self.bus.write_byte(0x48,0x04)
+        out = []
+        for ix in range(0,accuracy):
+            data = self.bus.read_i2c_block_data(0x48,int(sensor),32)[1:]
+            if  data[1] > 1 and data[2] > 1:
+                out.append(max(data))
+                counter -= 1
+                if counter < 1:
+                    break
+            if len(out) < 1:
+                out = [0]
+        #print out   # print printprintprintprintprint
+        return out
     @classmethod
-    def convertToVolts(self, readings):
-        vcc  = 2.22                                                                                                                                                           
-        factor = (vcc)/256                                                                                                                                                     
-        return (readings * factor) 
-
+    def avg(self,data):
+        data_ = 0
+        for i in data:
+            data_+= i
+        return data_/(len(data))
     @classmethod
-    def convertToAmpers(self, volts):
-        return volts * (1/30)
-
-    @classmethod
-    def convertToWats(self, ampers):
-        return ampers * self.supplayVolts
-
-    @classmethod
-    def readFromBus(self, sens):
-        self.bus.write_byte(0x48,sens)   
-        time.sleep(0.001)
-        return self.bus.read_byte(0x48)
-
-    @classmethod
-    def getData(self):
-        for sa in range(0,self.acc):
-            for ii in self.sens:
-                ints = self.readFromBus(int(self.sens[ii]))
-                volt = self.convertToVolts(ints)
-                
-                if volt > 0 or volt < 1.3 :
-                    print volt
-                    if self.dataOut[ii] < volt:
-                        self.dataOut[ii] = volt
-        for s in self.sens:
-            self.volts[s]  = self.dataOut[s]
-          
-            self.ampers[s] = self.convertToAmpers(self.volts[s])
-          
-            self.wats[s]   = self.convertToWats(self.ampers[s])
-          
+    def convert(self,data,type_):
+        factor = 2.29 / 256.0  # pfc factor
+        dataAvg = max(data)
+        if type_ != 0:
+            volts  = dataAvg * factor 
+            ampers = volts / 0.0333333
+            wats   = ampers * 235.0
+            volts  = 0
+        else: 
+            volts  = (dataAvg * factor )*190
+            ampers = 0
+            wats   = 0
+        return volts,ampers,wats
 
 
+   
     #read data from sensor
     @classmethod
     def __init__(self, *args):
-        
         for arg in args:
-            #try:
-                self.getData()
-                print self.wats[int(arg[1][-1:][0])]
-                maapidb.MaaPiDBConnection.insert_data(arg[0][0], self.wats[int(arg[1][-1:][0])]," " , True)
-
-            #except:
+            try:
+                data = self.read(int(arg[1][-1:][0]))
+                volt, amper, wat = self.convert(data,int(arg[1][-1:][0]))
+                if int(arg[1][-1:]) > 0:
+                    maapidb.MaaPiDBConnection.insert_data(arg[0],wat ," " , True)
+                else:
+                    
+                    maapidb.MaaPiDBConnection.insert_data(arg[0],volt," " , True)
+            except:
                 self._debug(1, "\tERROR reading values from dev: {0}".format(arg[1]))
                 maapidb.MaaPiDBConnection.insert_data(arg[0][0], 0," " , False)
 
