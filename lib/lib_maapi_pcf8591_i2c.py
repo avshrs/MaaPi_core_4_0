0136cf80319fc9f01e8a97463513acbd0a2cbb58
avshrs <sebastian.antosiak@gmail.com>
2019-01-11 00:01:01 +0100

    autocommit on change

--- lib/lib_maapi_pcf8591_i2c.py
+++ lib/lib_maapi_pcf8591_i2c.py
@@ -14,8 +14,6 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%m/%d/%Y %H:%M:%S')
 
-
-
 class class_get_values(object):
 
     @classmethod
@@ -34,17 +32,18 @@ class class_get_values(object):
 
 
     @classmethod
-    def read(self,sensor):
+    def read(self,sensor, address):
+
         counter = 20
         accuracy = 50
-        self.bus.write_byte(0x48,0x04)
+        self.bus.write_byte(0x4c,0x04)
         out = []
-        for ix in range(0,accuracy):
-            data = self.bus.read_i2c_block_data(0x48,int(sensor),32)[5:]
+        for ix in range(0,accuracy):      
+            data = self.bus.read_i2c_block_data(0x4c,int(sensor),32)[5:]
             if  data[12] > 0 and data[13] > 0 and data[14] > 0:
                 d = max(data)
 #                if sensor !=0 :
-#                    if d > 78:
+#                    if d > 78: 
 #                        continue
                 out.append(d)
                 counter -= 1
@@ -58,7 +57,7 @@ class class_get_values(object):
         if data:
             dataAvg = max(data)
             if type_ != 0:
-                volts  = dataAvg * factor 
+                volts  = (dataAvg * factor )-1.63
                 ampers = volts / 0.0333333
                 wats   = ampers * 235.0
                 volts  = 0
@@ -78,16 +77,18 @@ class class_get_values(object):
     @classmethod
     def __init__(self, *args):
         for arg in args:
-          try:
-                data = self.read(int(arg[1][-1:][0]))
+           # try:
+                nr = int(arg[1][-1:],10)
+                addr = int(arg[1][-8:-4],16)
+                data = self.read(nr, 0x4a)
                 volt, amper, wat = self.convert(data,int(arg[1][-1:][0]))
                 if int(arg[1][-1:]) > 0:
                     maapidb.MaaPiDBConnection.insert_data(arg[0],wat ," " , True)
                 else:
-                    
+                        
                     maapidb.MaaPiDBConnection.insert_data(arg[0],volt," " , True)
-          except:
-                self._debug(1, "\tERROR reading values from dev: {0}".format(arg[1]))
-		errr=0
-                maapidb.MaaPiDBConnection.insert_data(arg[0][0], errr," " , False)
+           # except:
+             #   self._debug(1, "\tERROR reading values from dev: {0}".format(arg))
+		  
+             #   maapidb.MaaPiDBConnection.insert_data(arg[0][0], 0," " , False)
 
