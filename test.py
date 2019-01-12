1f6cf5d842aaf7d3001a4be4610392a3aa76d9b1
avshrs <sebastian.antosiak@gmail.com>
2019-01-12 16:01:01 +0100

    autocommit on change

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 
 from lib.lib_maapi_onewire_pi_4_4  import class_get_values as bh
 
-a=[[2187, "28-04168575f9ff", 0,1],[9,"28-0000071ea24e", 0,1]]
-
-for i in a:
-  print a[1]
+a=(2187, "28-04168575f9ff", 0,1)
 
 bh(a)
