f1a13f58b6e7f1586d5b4e05a7dbc7a1894d98ab
avshrs <sebastian.antosiak@gmail.com>
2019-01-21 19:01:01 +0100

    autocommit on change

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.bind((HOST, PORT))
-s.listen(1)
+s.listen(0)
 conn, addr = s.accept()
 
 print('Connected by', addr)
