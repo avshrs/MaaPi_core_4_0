28b0d8b30c11a45b91ece14e5da57d185d6a82ce
avshrs <sebastian.antosiak@gmail.com>
2019-01-21 18:01:01 +0100

    autocommit on change

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
 
-x = np.linspace(0,2*np.pi,100)
-y = np.sin(x) + np.random.random(100) * 0.2
-yhat = savitzky_golay(y, 51, 3) # window size 51, polynomial order 3
+import socket
 
-plt.plot(x,y)
-plt.plot(x,yhat, color='red')
-plt.show()
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+
+print('Connected by', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        continue
+    conn.sendall(data) 
